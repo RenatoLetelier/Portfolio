5ced3351adceaf29fbf1c0b88112b6416b8dc1f8
RenatoLetelier <renato.letelier@gmail.com>
2025-08-04 19:04:01 -0400

    segundo commit, just fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <>Hola</>;
+  return (
+    <div className="App">
+      <h1>Bienvenido a la página de Renato Letelier!</h1>
+      <h2>Página en construcción...</h2>
+    </div>
+  );
 }
 
 export default App;
