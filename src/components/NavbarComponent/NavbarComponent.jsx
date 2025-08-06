2ea7ceaefe62ee6d237d7e53ed703b8f6de71745
RenatoLetelier <renato.letelier@gmail.com>
2025-08-05 23:22:35 -0400

    header and svgs

--- src/components/NavbarComponent/NavbarComponent.jsx
+++ src/components/NavbarComponent/NavbarComponent.jsx
@@ -1,21 +1,44 @@
+import githubIcon from "../../assets/icons/github.svg";
 import "./NavbarComponent.css";
 
 export default function Navbar() {
   return (
     <header className="navbar-container">
       <nav className="navbar">
-        <h1 className="title">RL</h1>
-        <ul className="nav-links">
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-          <li>
-            <a href="/projects">Projects</a>
-          </li>
-        </ul>
+        <div className="logo">
+          <h1 className="title">RL</h1>
+        </div>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+          </ul>
+        </div>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li>
+              <a href="/">In</a>
+            </li>
+            <li>
+              <li>
+                <img src={githubIcon} alt="GitHub" width="16" height="16" />
+              </li>
+            </li>
+            <li>
+              <a href="/">Language</a>
+            </li>
+            <li>
+              <a href="/">Theme</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
